4a9650e78b1158141c3f97ad800e8eeed64f9b3f
odrykrisztina <odrykrisztina6@gmail.com>
2025-12-07 12:43:23 +0100

    create start.bat to run servers

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 
-start "Laravel" cmd /k "cd /d backend\laravel && php artisan serve --port=8000"
-start "Angular" cmd /k "cd /d frontend\angular && npm start"
+start "Laravel" cmd /k "cd /d backend && php artisan serve --port=8000"
+start "Angular" cmd /k "cd /d frontend && npm start"
 exit
